09d336545319b410eb2fadc5fbd19f98c4b19212
fiaaaanso <sofian.taqui@etu.univ-lyon1.fr>
2026-05-25 16:04:11 +0200

    delete utils file

--- projet_elt/models/silver/check_anomalies.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='view', schema='silver') }}
-
-SELECT raison_anomalie, count(*) as nb_lignes
-FROM {{ ref('anomalies_adhesion') }}
-GROUP BY raison_anomalie
-ORDER BY nb_lignes DESC
